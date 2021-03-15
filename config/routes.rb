39d68e5248acb05b20b81b23beb302d2e47bccba
Nick McDaniel <nickamcd@gmail.com>
2021-03-15 12:08:59 -0700

    Updated root route to kittens#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :kittens
   
-  root to: 'KittensController#index'
+  root to: 'kittens#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
